2f2dd5fa9143cb652e418a00aa242c68e538118c
cpu <cpu@ciena.com>
2018-07-27 14:56:05 -0400

    use error wrapper library and the require testify API

--- headFirstPatterns/observer.go
+++ headFirstPatterns/observer.go
@@ -1,17 +1,27 @@
 package headFirstPatterns
 
 import (
+	"github.com/pkg/errors" // add context to the error stack
 	uuid "github.com/satori/go.uuid"
 )
 
+const (
+	MaxNumOfObservers = int(10)
+)
+
 type Subject interface {
-	RegisterObserver()
-	RemoveObserver()
-	NotifyObserver()
+	RegisterObserver(os Observer) error
+	/*
+		RemoveObserver()
+		NotifyObserver()
+	*/
 }
 
 type Observer interface {
-	Update()
+	Channel() chan Event
+	/*
+		Update()
+	*/
 }
 
 type Event struct {
@@ -20,5 +30,40 @@ type Event struct {
 }
 
 type subject struct {
-	observers []chan Event
+	observers         []chan Event
+	maxNumOfObservers int
+}
+
+type observer struct {
+	ch chan Event
+}
+
+// implement Subject
+func NewSubject(maxNumOfObservers int) (Subject, error) {
+	if maxNumOfObservers <= 0 {
+		return nil, errors.Errorf("invalid max number of observers. number: %d", maxNumOfObservers)
+	}
+	aSubject := &subject{
+		observers: make([]chan Event, maxNumOfObservers),
+	}
+	return aSubject, nil
+}
+
+func (s *subject) RegisterObserver(os Observer) error {
+	if os == nil {
+		return errors.New("nil obServer")
+	}
+	s.observers = append(s.observers, os.Channel())
+	return nil
+}
+
+// implement Observer
+func NewObserver() Observer {
+	return &observer{
+		ch: make(chan Event),
+	}
+}
+
+func (os *observer) Channel() chan Event {
+	return os.ch
 }
--- headFirstPatterns/observer_test.go
+++ headFirstPatterns/observer_test.go
@@ -1,1 +1,23 @@
 package headFirstPatterns_test
+
+import (
+	"testing"
+
+	"github.com/puxin71/DesignPatternInGo/headFirstPatterns"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewSubject(t *testing.T) {
+	aSubject, err := headFirstPatterns.NewSubject(-1)
+	require.NotNilf(t, err, "error message: %s", "expect an error")
+	require.Truef(t, aSubject == nil, "error message: %s", "expect nil object")
+
+	aSubject, err = headFirstPatterns.NewSubject(headFirstPatterns.MaxNumOfObservers)
+	require.Nil(t, err, "error message: %s", "expect no error")
+	require.Truef(t, aSubject != nil, "error message: %s", "expect concrete object")
+}
+
+func TestNewObserver(t *testing.T) {
+	aObserver := headFirstPatterns.NewObserver()
+	require.NotNilf(t, aObserver, "error message: %s", "expect concrete object")
+}
